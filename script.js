33de5976a7ef492674eb6a70ad7558091555100d
KILINC Bayram <badmusashii@gmail.com>
2023-06-20 15:42:44 +0200

    1er depot

---
+++ script.js
@@ -0,0 +1,4 @@
+var inputEmail = document.getElementById("id");
+var inputPassword = document.getElementById("password");
+console.log(inputPassword);
+var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
