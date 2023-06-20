3b9202bfa1c2320ba66db698bc92cb666310a81d
KILINC Bayram <badmusashii@gmail.com>
2023-06-20 16:22:01 +0200

    fini

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-var inputEmail = document.getElementById("id");
-var inputPassword = document.getElementById("password");
-console.log(inputPassword);
+let inputEmail = document.getElementById("email");
+let inputPassword = document.getElementById("password");
+let button = document.getElementById("button");
+let alertClass = document.getElementById("alert");
+
 var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+button.addEventListener("click", (e) => {
+  if (!emailRegex.test(inputEmail.value)) {
+    inputEmail.classList.add("error");
+    inputEmail.value = "Adresse Email Invalide";
+    setTimeout(() => {
+      inputEmail.value = "";
+    }, 2000);
+  } else if (inputPassword.value === "") {
+    inputPassword.classList.add("error");
+    alert.textContent = "Mot de pass requis";
+    setTimeout(() => {
+      alertClass.textContent = "";
+    }, 1500);
+  } else if (emailRegex.test(inputEmail.value) && inputPassword.value !== "") {
+    alert("Connexion en cours !!!!");
+  }
+  //   console.log(inputEmail.value);
+});
