94b37a9c9225af7b4404223b638688d4034843c8
Meranic <meranic77@gmail.com>
2023-06-21 09:33:18 +0200

    premier test de push et de conflit

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var inputEmail = document.getElementById("id");
-var inputPassword = document.getElementById("password");
+let inputEmail = document.getElementById("email");
+let inputPassword = document.getElementById("password");
+let submit = document.getElementById("button");
+let password = document.getElementById("fakepwd");
+let emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+console.log(password);
 console.log(inputPassword);
-var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+console.log(inputEmail);
+submit.addEventListener("click", () => {
+  if (!emailRegex.test(inputEmail.value) && inputPassword.value === "") {
+    inputEmail.classList.add("error");
+    inputEmail.value = "Casse non respectée";
+    inputPassword.classList.add("error");
+    password.textContent = "mauvais password";
+    setTimeout(reinitialise, 3000);
+  } else if (!emailRegex.test(inputEmail.value)) {
+    inputEmail.classList.add("error");
+    inputEmail.value = "Casse non respectée";
+    setTimeout(reinitialise, 3000);
+  } else if (inputPassword.value === "") {
+    inputPassword.classList.add("error");
+    password.textContent = "mauvais password";
+  }
+});
+function reinitialise() {
+  inputEmail.innerText = "Chuck@norris.com";
+}
