13c16ad197ba12382b87e28577fa34c4f0c002e2
KILINC Bayram (SNCF VOYAGEURS / DIRECTION DE LIGNE PARIS GRAND EST / EQUIPE TRAIN PARIS EST 4) <8103509m@commun.ad.sncf.fr>
2023-06-21 07:46:13 +0200

    essaie d'amelioration

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 let inputEmail = document.getElementById("email");
+let alertEmail = document.getElementById("email_alert_is_Invisible");
 let inputPassword = document.getElementById("password");
+let alertPassword = document.getElementById("password_alert_is_Invisible");
+console.log(alertPassword);
 let button = document.getElementById("button");
-let alertClass = document.getElementById("alert");
 
 var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
 button.addEventListener("click", (e) => {
   if (!emailRegex.test(inputEmail.value) && inputPassword.value === "") {
     inputEmail.classList.add("error");
-    inputEmail.value = "Adresse Email Invalide";
-    setTimeout(() => {
-      inputEmail.value = "";
-    }, 2000);
+    // inputEmail.value = "Adresse Email Invalide";
+    alertEmail.classList.add("email_alert_is_Visible");
+
     inputPassword.classList.add("error");
-    alertClass.textContent = "Mot de pass requis";
-    setTimeout(() => {
-      alertClass.textContent = "";
-    }, 1500);
+    // alertClass.textContent = "Mot de pass requis";
+    alertPassword.classList.add("password_alert_is_Visible");
   } else if (!emailRegex.test(inputEmail.value)) {
     inputEmail.classList.add("error");
     inputEmail.value = "Adresse Email Invalide";
@@ -32,5 +31,5 @@ button.addEventListener("click", (e) => {
   } else if (emailRegex.test(inputEmail.value) && inputPassword.value !== "") {
     alert("Connexion en cours !!!!");
   }
-  //   console.log(inputEmail.value);
+  console.log(inputEmail.value);
 });
