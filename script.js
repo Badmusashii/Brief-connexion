4b69350e3bbf97a54f6b53ddbfe9d622758a91d9
KILINC Bayram (SNCF VOYAGEURS / DIRECTION DE LIGNE PARIS GRAND EST / EQUIPE TRAIN PARIS EST 4) <8103509m@commun.ad.sncf.fr>
2023-06-21 09:30:09 +0200

    nouvelle essaie avec modif ok

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let inputEmail = document.getElementById("email");
-let alertEmail = document.getElementById("email_alert_is_Invisible");
+let alertEmail = document.getElementById("email_alert");
 let inputPassword = document.getElementById("password");
-let alertPassword = document.getElementById("password_alert_is_Invisible");
+let alertPassword = document.getElementById("password_alert");
+console.log(inputPassword);
 console.log(alertPassword);
 let button = document.getElementById("button");
 
@@ -10,26 +11,31 @@ var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 button.addEventListener("click", (e) => {
   if (!emailRegex.test(inputEmail.value) && inputPassword.value === "") {
     inputEmail.classList.add("error");
-    // inputEmail.value = "Adresse Email Invalide";
     alertEmail.classList.add("email_alert_is_Visible");
-
     inputPassword.classList.add("error");
-    // alertClass.textContent = "Mot de pass requis";
     alertPassword.classList.add("password_alert_is_Visible");
+    setTimeout(() => {
+      inputEmail.classList.remove("error");
+      alertEmail.classList.remove("email_alert_is_Visible");
+      inputPassword.classList.remove("error");
+      alertPassword.classList.remove("password_alert_is_Visible");
+    }, 2500);
   } else if (!emailRegex.test(inputEmail.value)) {
     inputEmail.classList.add("error");
-    inputEmail.value = "Adresse Email Invalide";
+    alertEmail.classList.add("email_alert_is_Visible");
     setTimeout(() => {
+      inputEmail.classList.remove("error");
+      alertEmail.classList.remove("email_alert_is_Visible");
       inputEmail.value = "";
     }, 2000);
   } else if (inputPassword.value === "") {
     inputPassword.classList.add("error");
-    alertClass.textContent = "Mot de pass requis";
+    alertPassword.classList.add("password_alert_is_Visible");
     setTimeout(() => {
-      alertClass.textContent = "";
+      inputPassword.classList.remove("error");
+      alertPassword.classList.remove("password_alert_is_Visible");
     }, 1500);
   } else if (emailRegex.test(inputEmail.value) && inputPassword.value !== "") {
     alert("Connexion en cours !!!!");
   }
-  console.log(inputEmail.value);
 });
